8eb7f8dc24d5d5b54f2a3f13fa22e17774cb7efe
DFST <mk@dfst.io>
2025-05-31 22:52:02 +0300

    docker 17

--- packages/avs/tee/src/nautilus-server/run.sh
+++ packages/avs/tee/src/nautilus-server/run.sh
@@ -78,6 +78,7 @@ mkdir -p /sys/fs/cgroup
 mount -t cgroup2 none /sys/fs/cgroup
 mkdir -p /run/docker /var/lib/docker
 addgroup -S docker
+export PATH=/usr/local/bin:$PATH
 dockerd --host=unix:///run/docker.sock \
  --iptables=false --ip-masq=false --bridge=none --exec-root=/run/docker &
 sleep 5
