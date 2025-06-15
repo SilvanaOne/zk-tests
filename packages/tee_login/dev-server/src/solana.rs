f3b83cef5878668f0359e6e58746bd0cd6ccd49d
DFST <mk@dfst.io>
2025-06-15 14:10:25 +0300

    flag

--- packages/tee_login/dev-server/src/solana.rs
+++ packages/tee_login/dev-server/src/solana.rs
@@ -1,3 +1,5 @@
+use bs58;
+use ed25519_dalek;
 use solana_sdk::pubkey::Pubkey;
 use solana_sdk::signature::Signature;
 use std::str::FromStr;
@@ -16,6 +18,13 @@ pub fn verify_signature(
     let message_bytes = message.as_bytes().to_vec();
     let signature = Signature::try_from(signature_bytes.as_slice())?;
     let is_valid = signature.verify(pubkey.as_ref(), &message_bytes);
+
+    // Decode the base58 encoded address to bytes
+    let pubkey_bytes = bs58::decode(address).into_vec()?;
+    let publickey = ed25519_dalek::PublicKey::from_bytes(&pubkey_bytes)?;
+    let dalek_signature = ed25519_dalek::Signature::from_bytes(&signature_bytes)?;
+    let dalek = publickey.verify_strict(&message_bytes, &dalek_signature);
+    println!("dalek: {:?}", dalek);
 
     Ok(is_valid)
 }
