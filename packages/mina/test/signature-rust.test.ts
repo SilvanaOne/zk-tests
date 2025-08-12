44edb73001de93ac82c5978fb179176ffd724099
DFST <mk@dfst.io>
2025-08-12 20:04:46 +0300

    rust signature

--- packages/mina/test/signature-rust.test.ts
+++ packages/mina/test/signature-rust.test.ts
@@ -2,6 +2,7 @@ import { describe, it } from "node:test";
 import assert from "node:assert";
 import { Poseidon, Signature, PublicKey, PrivateKey, Field } from "o1js";
 import bs58 from "bs58";
+import { initBlockchain } from "@silvana-one/mina-utils";
 
 describe("Signature", () => {
   it(`should calculate hash`, async () => {
@@ -12,12 +13,13 @@ describe("Signature", () => {
   });
 
   it(`should sign and verify`, async () => {
+    await initBlockchain("devnet");
     const privateKey = PrivateKey.fromBase58(
-      "EKEtdWo2dFqNx6qEhbeVaHkYbcnLhz3pXkS4mjQyfJdPFZB3onG5"
+      "EKFXH5yESt7nsD1TJy5WNb4agVczkvzPRVexKQ8qYdNqauQRA8Ef"
     );
     const publicKey = privateKey.toPublicKey();
     console.log("publicKey:", publicKey.toBase58());
-    const signature = Signature.create(privateKey, [1n, 2n].map(Field));
+    const signature = Signature.create(privateKey, [1n, 2n, 3n].map(Field));
     console.log("signature:", signature.toJSON());
     const signature_base58 = signature.toBase58();
     console.log("signature:", signature_base58);
